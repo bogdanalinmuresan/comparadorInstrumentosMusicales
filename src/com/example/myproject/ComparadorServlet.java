efc950e2fd9f14a94ed9827e68632e9d8b67c5e1
Bogdan Alin <alinugr@gmail.com>
2016-06-03 05:49:11 +0200

    terminado

--- src/com/example/myproject/ComparadorServlet.java
+++ src/com/example/myproject/ComparadorServlet.java
@@ -26,7 +26,7 @@ public class ComparadorServlet extends HttpServlet {
 		List<BookInfo>resuFnac;
 		List<BookInfo>resuAmazon;
 		String searchword = req.getParameter("keywords");
-		//System.out.println(searchword);
+		
 		
         EbayDriver driverEbay = new EbayDriver();
         fnacDriver driverFnac=new fnacDriver();
@@ -37,25 +37,7 @@ public class ComparadorServlet extends HttpServlet {
 			resuEbay=new ArrayList<BookInfo>(driverEbay.run(java.net.URLEncoder.encode(tag, "UTF-8")));
 			resuFnac=new ArrayList<BookInfo>(driverFnac.run(java.net.URLEncoder.encode(tag, "UTF-8")));
 			resuAmazon=new ArrayList<BookInfo>(driverAmazon.run(java.net.URLEncoder.encode(tag, "UTF-8")));
-			//String url="http://es.aliexpress.com/wholesale?catId=0&initiative_id=SB_20160531110333&SearchText=";
-			//url=url.concat(searchword);
 		
-			/*
-			Document doc=null;
-			try{
-				doc = Jsoup.connect(url).get();
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-			
-			Elements items = doc.getElementsByClass("item");
-			
-			for (Element item : items) {
-				String articulo = item.select(".item").first().html();
-				
-				}
-
-			*/
 			
 			req.setAttribute("articulosEbay", resuEbay);
 			req.setAttribute("articulosFnac", resuFnac);
@@ -69,43 +51,6 @@ public class ComparadorServlet extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			/*
-			try {
-				
-				String isbn = req.getParameter("isbn").trim().replace("-", "");
-				BookInfo book = null;
-				
-				if (isbn.length()>0) {
-					
-					book = checkAmazonOffers(isbn, book, ".com");
-					book = checkAmazonOffers(isbn, book, ".co.uk");
-					book = checkAmazonOffers(isbn, book, ".es");
-					book = checkAmazonOffers(isbn, book, ".de");
-					book = checkAmazonOffers(isbn, book, ".fr");
-					book = checkAmazonOffers(isbn, book, ".it");
-					book = checkAmazonOffers(isbn, book, ".ca");
-					
-					book = checkGoogleOffers(isbn, book, "US");
-					book = checkGoogleOffers(isbn, book, "UK");
-					book = checkGoogleOffers(isbn, book, "ES");
-					
-					
-				 
-				
-				
-					if (book!=null && book.isbn!=null) {
-						req.setAttribute("book", book);
-						RequestDispatcher rd = req.getRequestDispatcher("/book.jsp");
-						rd.forward(req, resp);
-					} else {				
-						resp.sendRedirect("/error.html");
-					}
-				 
-				} catch (Exception error) { 
-
-			    resp.sendRedirect("/error.html");
-			}
-		}
-*/
+			
 	}
 }//ComparadorServlet
